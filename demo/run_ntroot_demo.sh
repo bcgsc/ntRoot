601f3223fbc648d2927a888e5e3ed9ec1a16b78e
lcoombe <lauren.e.coombe@gmail.com>
2024-03-22 14:41:40 -0700

    Add genome test

--- demo/run_ntroot_demo.sh
+++ demo/run_ntroot_demo.sh
@@ -5,7 +5,7 @@ echo "Please ensure that ntRoot is on your PATH"
 
 set -eux -o pipefail
 
-echo "Running ntroots reads demo..."
+echo "Running ntRoot reads demo..."
 wget https://www.bcgsc.ca/downloads/btl/ntroot/reads_demo/ERR3239334.chr21_1.fq.gz
 wget https://www.bcgsc.ca/downloads/btl/ntroot/reads_demo/ERR3239334.chr21_2.fq.gz
 
@@ -19,3 +19,17 @@ else
 	echo "ntRoot reads test failed.. Please check your installation."
 	exit 1
 fi
+
+echo "Running ntRoot genome demo..."
+
+ntroot --draft chr21.fa --genome HuRef.chr21.fa -k 55 -l pop-spec-snp_chr21.vcf.gz
+
+prediction=$(HuRef.chr21_ntedit_k55_variants.vcf_ancestry-predictions_tile5000000.tsv | awk '{print $1}' |head -n 2 |tail -n 1)
+if [ ${prediction} == "EUR" ]; then
+	echo "ntRoot genome test successful!"
+else 
+	echo "ntRoot genome test failed.. Please check your installation."
+	exit 1
+fi
+
+echo "Done ntRoot tests!"
